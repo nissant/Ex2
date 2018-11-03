fc811b1131c26f365fa15043083d8f13d0a3defc
Nisan <tnisan@gmail.com>
2018-11-03 19:01:55 +0200

    added main code

--- ex2/main.c
+++ ex2/main.c
@@ -1,1 +1,16 @@
+/*
+Authors - Eli Slavutsky (308882992) & Nisan Tagar (302344031)
+Project Name - ex1. Pilots, Planes & Plans
+Description - In this project we assign for each destination a plane with a set of pilots (captain and first officer), with minimum hours, qualified to fly the plane.
+*/
+
 #include <stdio.h>
+
+#define _CRT_SECURE_NO_WARNINGS
+#define ERROR_MESSAGE "An error occurred during execution, couldn't complete the task!\n"
+
+void main(int argc, char *argv[]) {
+
+	printf("Eli and Nisan's git synced project! \n");
+
+}
