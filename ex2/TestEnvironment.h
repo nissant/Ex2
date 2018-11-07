972e90befa1c1717c470904d0ad42bff0ee90d82
Nisan <tnisan@gmail.com>
2018-11-07 16:52:35 +0200

    Bug fix

--- ex2/TestEnvironment.h
+++ ex2/TestEnvironment.h
@@ -26,8 +26,8 @@ Description - This program impelments an application testing tool. Test are run 
 
 
 typedef struct {
-	HANDLE test_thread_handles;
-	DWORD test_thread_id;
+	HANDLE test_thread_handles;						// Test thread data
+	DWORD test_thread_id;							// Test thread data
 	char app_path[MAX_LINE_LEN];					// String of size MAX_LINE_LEN holding app full path
 	char app_args[MAX_LINE_LEN];					// String of size MAX_LINE_LEN holding app complete argument string
 	char app_exp_results_path[MAX_LINE_LEN];		// String of size MAX_LINE_LEN holding app expected resuts full path
@@ -38,7 +38,7 @@ typedef struct {
 // Function Declarations -------------------------------------------------------
 
 char *trimwhitespace(char *str);
-int createAppTestList(char *tst_file_path[], test_app **head_list_ptr);
+int createAppTestList(char *tst_file_path, test_app **test_list_ptr);
 int createTestResults(char *report_file_path[]);
 static HANDLE CreateThreadSimple(LPTHREAD_START_ROUTINE p_start_routine,
 	LPDWORD p_thread_id);
