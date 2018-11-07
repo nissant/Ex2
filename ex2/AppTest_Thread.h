a76e3dc458da9b37505d2bafee2c099e05a3cbd9
Nisan <tnisan@gmail.com>
2018-11-07 13:29:42 +0200

    Project Structure

---
+++ ex2/AppTest_Thread.h
@@ -0,0 +1,8 @@
+/*
+Authors - Eli Slavutsky (308882992) & Nisan Tagar (302344031)
+Project Name - Ex2
+Description - This program impelments an application testing tool. Test are run in parallel using threads
+*/
+
+
+// Eli's Code Here
