b0153bea68aec5b265a3428672536f7df7a6f5bf
Nisan <tnisan@gmail.com>
2018-11-07 18:50:34 +0200

    Comments

--- ex2/AppTest_Thread.h
+++ ex2/AppTest_Thread.h
@@ -1,7 +1,8 @@
 /*
-Authors - Eli Slavutsky (308882992) & Nisan Tagar (302344031)
-Project Name - Ex2
-Description - This program impelments an application testing tool. Test are run in parallel using threads
+Authors			- Eli Slavutsky (308882992) & Nisan Tagar (302344031)
+Project Name	- Ex2
+Description		- This program impelments an application testing environment. Tests are run in parallel using threads and results are
+written in text output file once all threads have terminated.
 */
 
 
