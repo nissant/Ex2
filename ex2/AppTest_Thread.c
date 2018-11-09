8287859a2e7bac57817ccace536bb75125a6bd1c
Eli Slavutsky <eli308@gmail.com>
2018-11-09 18:26:17 +0200

    started my code

--- ex2/AppTest_Thread.c
+++ ex2/AppTest_Thread.c
@@ -7,6 +7,39 @@ written in text output file once all threads have terminated.
 
 
 #include "TestEnvironment.h"
+#include "AppTest_Thread.h"
+
+void runProc(test_app *test_list)
+{
+	PROCESS_INFORMATION procinfo;
+	DWORD				waitcode;
+	DWORD				exitcode;
+	BOOL				retVal;
+
+	retVal = CreateProcessSimple(command, &procinfo);
+
+}
+
+BOOL CreateProcessSimple(LPTSTR CommandLine, PROCESS_INFORMATION *ProcessInfoPtr)
+{
+	STARTUPINFO	startinfo = { sizeof(STARTUPINFO), NULL, 0 }; /* <ISP> here we */
+															  /* initialize a "Neutral" STARTUPINFO variable. Supplying this to */
+															  /* CreateProcess() means we have no special interest in this parameter. */
+															  /* This is equivalent to what we are doing by supplying NULL to most other */
+															  /* parameters of CreateProcess(). */
+
+	return CreateProcess(NULL, /*  No module name (use command line). */
+		CommandLine,			/*  Command line. */
+		NULL,					/*  Process handle not inheritable. */
+		NULL,					/*  Thread handle not inheritable. */
+		FALSE,					/*  Set handle inheritance to FALSE. */
+		NORMAL_PRIORITY_CLASS,	/*  creation/priority flags. */
+		NULL,					/*  Use parent's environment block. */
+		NULL,					/*  Use parent's starting directory. */
+		&startinfo,				/*  Pointer to STARTUPINFO structure. */
+		ProcessInfoPtr			/*  Pointer to PROCESS_INFORMATION structure. */
+	);
+}
 
 
 
